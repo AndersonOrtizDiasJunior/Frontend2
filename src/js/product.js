f03f4611570791be8cc3a626237d0b85b81c1d77
AndersonOrtizDiasJunior <anderson_ortiz@hotmail.com.br>
2023-11-06 08:04:27 -0300

    feat:fix add to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+var products = getLocalStorage("so-cart")
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+function addProductToCart(items) {
+  setLocalStorage("so-cart", items);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  products.push(product)
+  addProductToCart(products);
 }
 
 // add listener to Add to Cart button
